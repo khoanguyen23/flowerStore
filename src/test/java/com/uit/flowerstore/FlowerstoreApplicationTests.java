aab9b8ab30ba34ccd073b58328968bbde61022aa
khoanguyen23 <khoahoang151@gmail.com>
2023-03-28 21:08:20 +0700

    Initialize project with Spring Boot

---
+++ src/test/java/com/uit/flowerstore/FlowerstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uit.flowerstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
