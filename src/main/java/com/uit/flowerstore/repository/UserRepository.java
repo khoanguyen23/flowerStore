bdc33f351fe6c6eb507fa1e30416a9b9f21b77ef
khoanguyen23 <khoahoang151@gmail.com>
2023-06-14 19:55:32 +0700

    [add] delete userPayment

--- src/main/java/com/uit/flowerstore/repository/UserRepository.java
+++ src/main/java/com/uit/flowerstore/repository/UserRepository.java
@@ -14,6 +14,7 @@ import com.uit.flowerstore.domain.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
   Optional<User> findByUsername(String username);
+  Optional<User> findByEmail(String email);
 
   Boolean existsByUsername(String username);
 
